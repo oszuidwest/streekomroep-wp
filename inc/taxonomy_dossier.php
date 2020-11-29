34d02d0ccd3ff7ef24c1b86611a706d8b6a1251e
rmens <r.mens@me.com>
2020-11-29 18:07:20 +0100

    Taxonomieen voor regio's en dossier

---
+++ inc/taxonomy_dossier.php
@@ -0,0 +1,30 @@
+// Register Dossier Taxonomy
+function taxonomy_dossier() {
+
+	$labels = array(
+		'name'                       => 'Dossiers',
+		'singular_name'              => 'Dossier',
+		'menu_name'                  => 'Dossiers',
+		'all_items'                  => 'Alle dossiers',
+		'new_item_name'              => 'Nieuw dossier',
+		'add_new_item'               => 'Voeg nieuw dossier toe',
+		'edit_item'                  => 'Bewerk dossier',
+		'update_item'                => 'Update dossier',
+		'view_item'                  => 'Bekijk dossier',
+		'no_terms'                   => 'Geen dossiers',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'show_in_rest'               => true,
+		'rest_base'                  => 'dossier',
+	);
+	register_taxonomy( 'dossier', array( 'post', ' fragment' ), $args );
+
+}
+add_action( 'init', 'taxonomy_dossier', 0 );
---
+++ inc/taxonomy_regio.php
@@ -0,0 +1,30 @@
+// Register Regio Taxonomy
+function taxonomy_regio() {
+
+	$labels = array(
+		'name'                       => 'Regio\'s',
+		'singular_name'              => 'Regio',
+		'menu_name'                  => 'Regio\'s',
+		'all_items'                  => 'Alle regio\'s',
+		'new_item_name'              => 'Nieuwe regio',
+		'add_new_item'               => 'Voeg nieuwe regio toe',
+		'edit_item'                  => 'Bewerk regio',
+		'update_item'                => 'Update regio',
+		'view_item'                  => 'Bekijk regio',
+		'no_terms'                   => 'Geen regio\'s',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'show_in_rest'               => true,
+		'rest_base'                  => 'regio',
+	);
+	register_taxonomy( 'regio', array( 'post', ' fragment', ' agenda' ), $args );
+
+}
+add_action( 'init', 'taxonomy_regio', 0 );
