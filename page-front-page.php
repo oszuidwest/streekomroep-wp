1e4d3670bc3ca59dfa005a3d830f3ec1f654541d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-12-27 18:07:44 +0100

    Add php code for front page

---
+++ page-front-page.php
@@ -0,0 +1,7 @@
+<?php
+$context = Timber::context();
+
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+$context['options'] = new Timber\Post('options');
+Timber::render(array('page-' . $timber_post->post_name . '.twig', 'page.twig'), $context);
