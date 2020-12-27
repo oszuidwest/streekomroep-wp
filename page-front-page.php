b9cd904683f811b868a3bb21bf67d75cad8a2653
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-12-27 20:47:52 +0100

    Get popular posts

--- page-front-page.php
+++ page-front-page.php
@@ -4,4 +4,12 @@ $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 $context['options'] = get_fields('options');
+foreach ($context['options']['blokken_sidebar'] as &$block) {
+
+    switch ($block['acf_fc_layout']) {
+        case 'blok_meest_gelezen':
+            $block['posts'] = Timber::get_posts(['tag' => 'populair']);
+            break;
+    }
+}
 Timber::render(array('page-' . $timber_post->post_name . '.twig', 'page.twig'), $context);
