6ee93198019070deefa47476edd0e8b0e525221e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-12-27 18:14:55 +0100

    get options

--- page-front-page.php
+++ page-front-page.php
@@ -3,5 +3,5 @@ $context = Timber::context();
 
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
-$context['options'] = new Timber\Post('options');
+$context['options'] = get_fields('options');
 Timber::render(array('page-' . $timber_post->post_name . '.twig', 'page.twig'), $context);
