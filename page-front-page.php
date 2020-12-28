7d4ecc11e4d6783515c0f9b2d459fffdc1da4bb9
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2020-12-28 01:57:10 +0100

    Get articles for list

--- page-front-page.php
+++ page-front-page.php
@@ -5,10 +5,21 @@ $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 $context['options'] = get_fields('options');
 foreach ($context['options']['blokken_sidebar'] as &$block) {
-
     switch ($block['acf_fc_layout']) {
         case 'blok_meest_gelezen':
             $block['posts'] = Timber::get_posts(['tag' => 'populair']);
+            break;
+    }
+}
+
+foreach ($context['options']['blokken_voorpagina'] as &$block) {
+    switch ($block['acf_fc_layout']) {
+        case 'blok_artikelen_lijst':
+            $block['posts'] = Timber::get_posts([
+                'posts_per_page' => $block['artikel_lijst_aantal_artikelen'],
+                'offset' => $block['artikel_lijst_offset'],
+                'ignore_sticky_posts' => true,
+            ]);
             break;
     }
 }
