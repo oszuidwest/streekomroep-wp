e7a0d445382ccb1186e4471dbdf9b95ce177f1cc
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-01-15 14:22:09 +0100

    Add FM Guide

--- wp-page-fm-guide.php
+++ wp-page-fm-guide.php
@@ -15,4 +15,61 @@ $shows = Timber::get_posts([
 ]);
 $context['shows'] = $shows;
 
+class Day
+{
+    public $name;
+    public $broadcasts = [];
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function add(Broadcast $param)
+    {
+        $this->broadcasts[] = $param;
+        usort($this->broadcasts, [Broadcast::class, 'sort']);
+    }
+}
+
+class Broadcast
+{
+    public $show;
+    public $startTime;
+    public $endTime;
+
+    public function __construct($show, $startTime, $endTime)
+    {
+        $this->show = $show;
+        $this->startTime = $startTime;
+        $this->endTime = $endTime;
+    }
+
+    public static function sort(Broadcast $lhs, Broadcast $rhs)
+    {
+        return strcmp($lhs->startTime, $rhs->startTime);
+    }
+}
+
+
+$days = [];
+
+foreach ($shows as $show) {
+    $rules = $show->meta('fm_show_programmatie');
+    if ($rules) {
+        foreach ($rules as $rule) {
+            foreach ($rule['fm_show_dagen'] as $day) {
+                if (!isset($days[$day])) {
+                    $days[$day] = new Day($day);
+                }
+
+                $days[$day]->add(new Broadcast($show, $rule['fm_show_starttijd'], $rule['fm_show_eindtijd']));
+            }
+        }
+    }
+}
+
+
+$context['days'] = $days;
+
 Timber::render(array('page-fm-guide.twig', 'page.twig'), $context);
