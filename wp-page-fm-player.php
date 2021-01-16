abbae3d7b40b63096a8b1b12b0ab3c234ef7c302
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-01-16 10:13:07 +0100

    Add empty FM player

--- wp-page-fm-player.php
+++ wp-page-fm-player.php
@@ -5,5 +5,11 @@
 
 $context = Timber::context();
 $context['post'] = new Timber\Post();
+$context['current'] = new Timber\Post();
+$context['next'] = new Timber\Post();
+$context['options'] = get_fields('option');
 
-Timber::render(['page-fm-live.twig', 'page.twig'], $context);
+wp_enqueue_style('wp-mediaelement');
+wp_enqueue_script('wp-mediaelement');
+
+Timber::render(['page-fm-player.twig', 'page.twig'], $context);
