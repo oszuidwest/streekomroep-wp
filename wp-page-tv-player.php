08acdc8fb32283c4a6582c5047887446c50b8be3
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-01-19 15:58:46 +0100

    Make video players responsive

--- wp-page-tv-player.php
+++ wp-page-tv-player.php
@@ -10,6 +10,7 @@ $context['options'] = get_fields('option');
 wp_enqueue_style('video.js', 'https://unpkg.com/video.js/dist/video-js.min.css');
 wp_enqueue_script('video.js', 'https://unpkg.com/video.js/dist/video.min.js');
 
+
 wp_enqueue_style('wp-mediaelement');
 wp_enqueue_script('wp-mediaelement');
 
