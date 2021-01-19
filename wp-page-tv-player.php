baad690d8dee584229eba7db981c486445b0082b
Raymon Mens <r.mens@me.com>
2021-01-19 17:06:25 +0100

    Fix small typo

--- wp-page-tv-player.php
+++ wp-page-tv-player.php
@@ -7,7 +7,7 @@ $context = Timber::context();
 $context['post'] = new Timber\Post();
 $context['options'] = get_fields('option');
 
-wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.9.7/video-js.css'');
+wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.9.7/video-js.css');
 wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.9.7/video.min.js');
 
 
