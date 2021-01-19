a028caab5dd642bee99305ffff357de98cdaaa0d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-01-19 15:22:32 +0100

    Add video.js

--- wp-page-fm-player.php
+++ wp-page-fm-player.php
@@ -9,7 +9,7 @@ $context['current'] = new Timber\Post();
 $context['next'] = new Timber\Post();
 $context['options'] = get_fields('option');
 
-wp_enqueue_style('wp-mediaelement');
-wp_enqueue_script('wp-mediaelement');
+wp_enqueue_style('video.js', 'https://unpkg.com/video.js/dist/video-js.min.css');
+wp_enqueue_script('video.js', 'https://unpkg.com/video.js/dist/video.min.js');
 
 Timber::render(['page-fm-player.twig', 'page.twig'], $context);
--- wp-page-tv-player.php
+++ wp-page-tv-player.php
@@ -7,4 +7,7 @@ $context = Timber::context();
 $context['post'] = new Timber\Post();
 $context['options'] = get_fields('option');
 
+wp_enqueue_style('video.js', 'https://unpkg.com/video.js/dist/video-js.min.css');
+wp_enqueue_script('video.js', 'https://unpkg.com/video.js/dist/video.min.js');
+
 Timber::render(['page-tv-player.twig', 'page.twig'], $context);
