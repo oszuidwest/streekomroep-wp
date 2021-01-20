2895f9bc5a26185a62726dd1fa022eb054bf0677
rmens <r.mens@me.com>
2021-01-20 16:38:58 +0100

    Upgrade VideoJS to latest beta

--- wp-page-fm-player.php
+++ wp-page-fm-player.php
@@ -9,7 +9,7 @@ $context['current'] = new Timber\Post();
 $context['next'] = new Timber\Post();
 $context['options'] = get_fields('option');
 
-wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.9.7/video-js.css');
-wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.9.7/video.min.js');
+wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.11.2/video-js.css');
+wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.11.2/video.min.js');
 
 Timber::render(['page-fm-player.twig', 'page.twig'], $context);
