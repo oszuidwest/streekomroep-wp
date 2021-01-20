7e154a87b41d509a22129a0f0ae2139700e3bf9f
rmens <r.mens@me.com>
2021-01-20 16:35:13 +0100

    Remove MediaElementJS
    We standardize om VideoJS for live for now.

--- wp-page-fm-player.php
+++ wp-page-fm-player.php
@@ -12,7 +12,4 @@ $context['options'] = get_fields('option');
 wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.9.7/video-js.css');
 wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.9.7/video.min.js');
 
-wp_enqueue_style('wp-mediaelement');
-wp_enqueue_script('wp-mediaelement');
-
 Timber::render(['page-fm-player.twig', 'page.twig'], $context);
--- wp-page-tv-player.php
+++ wp-page-tv-player.php
@@ -10,8 +10,4 @@ $context['options'] = get_fields('option');
 wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.9.7/video-js.css');
 wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.9.7/video.min.js');
 
-
-wp_enqueue_style('wp-mediaelement');
-wp_enqueue_script('wp-mediaelement');
-
 Timber::render(['page-tv-player.twig', 'page.twig'], $context);
