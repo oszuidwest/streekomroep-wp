77cfd411562303b904a160b80587b5e29e93f963
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-02-05 03:41:00 +0100

    Get largest thumbnail

--- vimeo-thumbnail.php
+++ vimeo-thumbnail.php
@@ -22,4 +22,43 @@ add_action('acf/save_post', function (int $post_ID) {
 
     $duration = $vimeo->duration;
     update_field('fragment_duur', $duration, $post_ID);
+
+    $thumb = get_post_thumbnail_id($post_ID);
+    $key = $vimeo->pictures->resource_key;
+    if ($thumb != 0) {
+        dd('Non-zero thumbnail found');
+        return;
+    }
+
+    $bestPic = null;
+    $bestWidth = -1;
+    foreach ($vimeo->pictures->sizes as $size) {
+        if ($size->width > $bestWidth) {
+            $bestPic = $size;
+        }
+    }
+
+    if ($bestPic === null)
+        return;
+
+    $tempPath = download_url($bestPic->link);
+    if ($tempPath instanceof WP_Error) {
+        dd($tempPath);
+        return;
+    }
+
+    $path = parse_url($bestPic->link, PHP_URL_PATH);
+
+    $file = [
+        'name' => basename($path),
+        'tmp_name' => $tempPath,
+    ];
+    $thumbnail_id = media_handle_sideload($file, $post_ID);
+    if ($thumbnail_id instanceof WP_Error) {
+        dd($thumbnail_id);
+        return;
+    }
+
+    set_post_thumbnail($post_ID, $thumbnail_id);
+    update_post_meta($thumbnail_id, 'vimeo_resource_key', $key);
 });
