26bff309f86277c745d819e72781a9ab9909b8c6
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-02-07 10:40:11 +0100

    Add 'lees meer' embed

--- functions.php
+++ functions.php
@@ -14,31 +14,31 @@
  * plug-in, you can safely delete this block.
  */
 $composer_autoload = __DIR__ . '/vendor/autoload.php';
-if ( file_exists( $composer_autoload ) ) {
-	require_once $composer_autoload;
-	$timber = new Timber\Timber();
+if (file_exists($composer_autoload)) {
+    require_once $composer_autoload;
+    $timber = new Timber\Timber();
 }
 
 /**
  * This ensures that Timber is loaded and available as a PHP class.
  * If not, it gives an error message to help direct developers on where to activate
  */
-if ( ! class_exists( 'Timber' ) ) {
+if (!class_exists('Timber')) {
 
-	add_action(
-		'admin_notices',
-		function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-		}
-	);
+    add_action(
+        'admin_notices',
+        function () {
+            echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+        }
+    );
 
-	add_filter(
-		'template_include',
-		function( $template ) {
-			return get_stylesheet_directory() . '/static/no-timber.html';
-		}
-	);
-	return;
+    add_filter(
+        'template_include',
+        function ($template) {
+            return get_stylesheet_directory() . '/static/no-timber.html';
+        }
+    );
+    return;
 }
 
 if (!class_exists('ACF')) {
@@ -56,7 +56,7 @@ require 'vimeo-thumbnail.php';
 /**
  * Sets the directories (inside your theme) to find .twig files
  */
-Timber::$dirname = array( 'templates', 'views' );
+Timber::$dirname = array('templates', 'views');
 
 /**
  * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
@@ -75,74 +75,76 @@ add_filter('Timber\PostClassMap', function () {
 new \Streekomroep\Site();
 
 /**
- * Include ACF Fields. These are saved as local JSON 
+ * Include ACF Fields. These are saved as local JSON
  * This is not a function of Timber so we declare them afer the Timber specific functions
  */
 if (function_exists('get_field')) {
 
-  add_filter('acf/settings/save_json', 'streekomroep_acf_json_save_point');
+    add_filter('acf/settings/save_json', 'streekomroep_acf_json_save_point');
 
-  function streekomroep_acf_json_save_point( $path ) {
+    function streekomroep_acf_json_save_point($path)
+    {
 
-      // update path
-      $path = get_stylesheet_directory() . '/streekomroep-acf-json';
+        // update path
+        $path = get_stylesheet_directory() . '/streekomroep-acf-json';
 
-      // return
-      return $path;
+        // return
+        return $path;
 
-  }
+    }
 
-  add_filter('acf/settings/load_json', 'streekomroep_acf_json_load_point');
+    add_filter('acf/settings/load_json', 'streekomroep_acf_json_load_point');
 
-  function streekomroep_acf_json_load_point( $paths ) {
+    function streekomroep_acf_json_load_point($paths)
+    {
 
-      // remove original path (optional)
-      unset($paths[0]);
+        // remove original path (optional)
+        unset($paths[0]);
 
-      // append path
-      $paths[] = get_stylesheet_directory() . '/streekomroep-acf-json';
+        // append path
+        $paths[] = get_stylesheet_directory() . '/streekomroep-acf-json';
 
 
-      // return
-      return $paths;
+        // return
+        return $paths;
 
-  }
+    }
 }
 
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page(array(
-		'page_title' 	=> 'Radio Instellingen',
-		'menu_title'	=> 'Radio Instellingen',
-		'menu_slug' 	=> 'radio-instellingen',
-		'capability'	=> 'edit_posts',
-		'icon_url' => 'dashicons-playlist-audio',
-		'redirect'	=> false
-	));	
+if (function_exists('acf_add_options_page')) {
+
+    acf_add_options_page(array(
+        'page_title' => 'Radio Instellingen',
+        'menu_title' => 'Radio Instellingen',
+        'menu_slug' => 'radio-instellingen',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-playlist-audio',
+        'redirect' => false
+    ));
 }
 
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page(array(
-		'page_title' 	=> 'TV Instellingen',
-		'menu_title'	=> 'TV Instellingen',
-		'menu_slug' 	=> 'tv-instellingen',
-		'capability'	=> 'edit_posts',
-		'icon_url' => 'dashicons-format-video',
-		'redirect'	=> false
-	));	
+if (function_exists('acf_add_options_page')) {
+
+    acf_add_options_page(array(
+        'page_title' => 'TV Instellingen',
+        'menu_title' => 'TV Instellingen',
+        'menu_slug' => 'tv-instellingen',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-format-video',
+        'redirect' => false
+    ));
 }
 
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page(array(
-		'page_title' 	=> 'Desking',
-		'menu_title'	=> 'Desking',
-		'menu_slug' 	=> 'desking',
-		'capability'	=> 'edit_posts',
-		'icon_url' => 'dashicons-layout',
-		'redirect'	=> false
-	));	
+if (function_exists('acf_add_options_page')) {
+
+    acf_add_options_page(array(
+        'page_title' => 'Desking',
+        'menu_title' => 'Desking',
+        'menu_slug' => 'desking',
+        'capability' => 'edit_posts',
+        'icon_url' => 'dashicons-layout',
+        'redirect' => false
+    ));
 }
 
 function zw_parse_query(WP_Query $query)
@@ -294,27 +296,69 @@ function zw_get_socials()
     return $out;
 }
 
-class Jetpack_Options{
-    public static function get_option_and_ensure_autoload() {
+wp_embed_register_handler('zw-readmore', '#^(.*)$#', 'zw_embed_handler');
+
+function zw_embed_handler($matches, $attr, $url, $rawattr)
+{
+    $self = parse_url(get_site_url(), PHP_URL_HOST);
+    $host = parse_url($url, PHP_URL_HOST);
+    if (!in_array($host, [$self, 'www.zuidwestfm.nl']))
+        return false;
+
+    $postId = url_to_postid($url);
+    if ($postId === 0)
+        return false;
+
+    if (get_post_type($postId) !== 'post')
+        return false;
+
+    return '[zw_embed]' . $url . '[/zw_embed]';
+}
+
+add_shortcode('zw_embed', 'zw_embed');
+
+function zw_embed($atts, $content, $shortcode_tag)
+{
+    $url = $content;
+
+    $postId = url_to_postid($url);
+    if ($postId === 0)
+        return false;
+
+    $post = Timber::get_post($postId);
+    $html = Timber::compile('embed.twig', ['post' => $post]);
+
+    return $html;
+}
+
+class Jetpack_Options
+{
+    public static function get_option_and_ensure_autoload()
+    {
         return 'rectangular';
     }
 
-    public static function get_option($option) {
+    public static function get_option($option)
+    {
         return get_option($option);
     }
 }
 
-class Jetpack{
-    public static function get_content_width() {
+class Jetpack
+{
+    public static function get_content_width()
+    {
         return 672;
     }
 
-    public static function get_active_modules() {
+    public static function get_active_modules()
+    {
         return ['carousel'];
     }
 }
 
-function jetpack_photon_url ($image_url, $args = array(), $scheme = null ) {
+function jetpack_photon_url($image_url, $args = array(), $scheme = null)
+{
 //    var_dump(__FUNCTION__);
     return $image_url;
 }
