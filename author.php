6ef9c029521bb23feceb1e620402d320aa6aea90
Raymon Mens <r.mens@me.com>
2021-02-09 16:37:09 +0100

    Update author.php

--- author.php
+++ author.php
@@ -16,6 +16,6 @@ $context['posts'] = new Timber\PostQuery();
 if ( isset( $wp_query->query_vars['author'] ) ) {
 	$author            = new Timber\User( $wp_query->query_vars['author'] );
 	$context['author'] = $author;
-	$context['title']  = 'Author Archives: ' . $author->name();
+	$context['title']  = 'Artikelen geschreven door ' . $author->name();
 }
 Timber::render( array( 'author.twig', 'archive.twig' ), $context );
