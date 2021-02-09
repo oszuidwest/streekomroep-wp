8403c619953ca3463eb81ac4d3e1bd6f28f49fa3
rmens <r.mens@me.com>
2021-02-09 16:21:45 +0100

    Upgrade videojs

--- wp-page-fm-player.php
+++ wp-page-fm-player.php
@@ -8,7 +8,7 @@ $context['post'] = new Timber\Post();
 $context['current'] = new Timber\Post();
 $context['next'] = new Timber\Post();
 
-wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.11.2/video-js.css');
-wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.11.2/video.min.js');
+wp_enqueue_style('video.js', 'https://vjs.zencdn.net/7.11.5/video-js.css');
+wp_enqueue_script('video.js', 'https://vjs.zencdn.net/7.11.5/video.min.js');
 
 Timber::render(['page-fm-player.twig', 'page.twig'], $context);
