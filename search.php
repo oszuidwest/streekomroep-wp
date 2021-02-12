0a403c12a9dec27bb05c93d1c016d90a54decee3
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-02-12 14:35:11 +0100

    Make search results into grid

--- search.php
+++ search.php
@@ -12,7 +12,6 @@
 $templates = array( 'search.twig', 'archive.twig', 'index.twig' );
 
 $context          = Timber::context();
-$context['title'] = 'Search results for ' . get_search_query();
 $context['posts'] = new Timber\PostQuery();
 
 Timber::render( $templates, $context );
