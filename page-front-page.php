25a00ed456ebeb7db5fdbc1329a013ddd470e71b
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-03-12 14:30:28 +0100

    Ignore videos not assigned to a folder

--- page-front-page.php
+++ page-front-page.php
@@ -56,7 +56,7 @@ foreach ($context['options']['desking_blokken_voorpagina'] as &$block) {
 
             $videos = [];
             foreach ($vimeo as $video) {
-                if (!isset($video->parent_folder)) continue;
+                if ($video->parent_folder === null) continue;
 
                 $projectId = basename($video->parent_folder->uri);
                 $args = [
