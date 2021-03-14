9d63cb5051a27846fd079c6e3f138941341d699e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-03-14 14:54:20 +0100

    Reformat zw_remove_wp_block_library_css

--- functions.php
+++ functions.php
@@ -622,12 +622,14 @@ add_filter('img_caption_shortcode_width', '__return_false');
  * Remove block editor (Gutenberg) CSS
  * This function dequeues the CSS for the block editor which this theme does not use
  */
-function zw_remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    wp_dequeue_style( 'wp-block-library-theme' );
-    wp_dequeue_style( 'wc-block-style' );
-} 
-add_action( 'wp_enqueue_scripts', 'zw_remove_wp_block_library_css', 100 );
+function zw_remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-block-style');
+}
+
+add_action('wp_enqueue_scripts', 'zw_remove_wp_block_library_css', 100);
 
 class Jetpack
 {
