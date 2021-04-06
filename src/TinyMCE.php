56831824cf405bd27b2a6eb6b1fe8a0b4e437938
Raymon Mens <r.mens@me.com>
2021-04-06 19:09:58 +0200

    Create TinyMCE.php

---
+++ src/TinyMCE.php
@@ -0,0 +1,6 @@
+function zw_TinyMCE( $in ) {
+	$in['paste_remove_styles'] = true;
+	$in['paste_remove_spans'] = false;
+	$in['paste_strip_class_attributes'] = 'all';
+}
+add_filter( 'tiny_mce_before_init', 'zw_TinyMCE' );
