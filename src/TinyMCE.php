58ac5274bf3ce204be81a632d6e0cdb0ccd4892c
Raymon Mens <r.mens@me.com>
2021-04-06 19:41:20 +0200

    Update TinyMCE.php

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -1,7 +1,5 @@
 <?php
 function zw_TinyMCE( $in ) {
-	$in['paste_remove_styles'] = true;
-	$in['paste_remove_spans'] = false;
-	$in['paste_strip_class_attributes'] = 'all';
+	$in['paste_as_text'] = true;
 }
 add_filter( 'tiny_mce_before_init', 'zw_TinyMCE' );
