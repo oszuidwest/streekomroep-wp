98b9cbaf71941e67af7fbfaee3402c81f2d45e13
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-05-13 22:59:38 +0200

    Add video to webpage

--- functions.php
+++ functions.php
@@ -597,6 +597,7 @@ if (defined('WP_DEBUG') && WP_DEBUG) {
     add_filter('yoast_seo_development_mode', '__return_true');
 }
 add_filter('wpseo_schema_graph_pieces', 'add_custom_schema_piece', 11, 2);
+add_filter('wpseo_schema_webpage', 'zw_seo_add_framgent_video', 10, 2);
 add_filter('wpseo_schema_article', 'zw_seo_article_add_region', 10, 2);
 
 function zw_fragment_get_file_url($post_id)
@@ -676,6 +677,21 @@ function add_custom_schema_piece($pieces, $context)
     $pieces[] = new VideoObject();
 
     return $pieces;
+}
+
+function zw_seo_add_framgent_video($data, $context)
+{
+    if (!is_singular('fragment'))
+        return $data;
+
+    $type = get_field('fragment_type', false, false);
+    if ($type !== 'Video')
+        return $data;
+
+    $data['video'] = [
+        ['id' => $context->canonical . '#video']
+    ];
+    return $data;
 }
 
 function zw_seo_article_add_region($data, $context)
